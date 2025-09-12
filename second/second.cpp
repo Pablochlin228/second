39e1e8642ed1ee5f491c646735f9795e62276509
Cerk_di95 <Cerk_di95@29-7>
2025-09-12 10:20:35 +0300

    d

--- second/second.cpp
+++ second/second.cpp
@@ -2,69 +2,39 @@
 #include<time.h>
 using namespace std;
 
-class Test
-{
-	int temp;
-	int temp2;
-public:
-	/*Test()
-	{
-		temp = 0;
-	}*/
-	Test(int t, int t2)
-	{
-		temp = t;
-		temp2 = t2;
-	}
-	void Show()
-	{
-		cout << temp << endl;
-	}
-	int Gettemp()
-	{
-		return temp;
-	}
-};
 class Point
 {
-	const int x;
+private:
+	int x;
 	int y;
-	int& z;
-	Test obj;
 public:
-
-	Point(int x1, int y1, int z1, int t1, int t2) :x(x1), z(z1), obj(t1, t2) // инициализаторы
-	{
-		cout << "Construct by 4 param\n";
-		y = y1;
-
-	}
-	void Init()
-	{
-		//	x = rand() % 10;
-		y = rand() % 10;
-	}
-	void Output()const
-	{
-		cout << "X: " << x << "\tY: " << y << endl;
-	}
-	int Getobj()
-	{
-		return obj.Gettemp();
-	}
 };
 
 int main()
 {
 	srand(unsigned(time(0)));
-	Point a(10, 1, 100, 1000, 8);
-	Point b(20, 2, 200, 2000, 9);
-	cout << b.Getobj() << endl;
+	const int size = 10;
+	int arr[size];
+	int min = 0;
+	int max = 0;
 
-
-
-	a.Output();
-
-
-	system("pause");
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 20;
+		if (min > arr[i] || min == 0)
+		{
+			min = arr[i];
+		}
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	cout << "Min: " << min << endl;
+	cout << "Max: " << max << endl;
 }
